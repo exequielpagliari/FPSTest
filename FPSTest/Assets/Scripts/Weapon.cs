ae43cac9e686f7da8692f7a1f88d3812002b7d5c
Exequiel Pagliari <exepa@hotmail.com>
2025-05-02 23:01:28 -0300

    New Systems

--- FPSTest/Assets/Scripts/Weapon.cs
+++ FPSTest/Assets/Scripts/Weapon.cs
@@ -1,15 +1,35 @@
 using UnityEngine;
 
 
+
+
 public class Weapon : MonoBehaviour
 {
     
-    
+    public enum TypeAttack 
+    {
+        Ray,
+        Projectil,
+        Mele
+    };
+    [Header("Type Of Weapon")]
+    public TypeAttack typeAttack = TypeAttack.Ray;
+    public LayerMask layerMask;
+
     public GameObject bullet;
     public Transform gunTransform;
 
     public float shootTime = 1;
     public float timeElapsed = 0;
+
+    [Header("EffectsWeapon")]
+    public GameObject goEffect;
+    public ParticleSystem shootParticles;
+    public GameObject shootLight;
+    public WeaponRecoil weaponRecoil;
+
+    [Header("Config Raycast")]
+    public Camera camGun;
 
     void Start()
     {
@@ -18,13 +38,48 @@ public class Weapon : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButton(0))
+
+        if (Input.GetButton("Fire1"))
         {
-            if(timeElapsed < Time.time)
+
+            if (timeElapsed < Time.time && typeAttack == TypeAttack.Projectil)
             {
+                shootParticles.Play();
+                shootLight.SetActive(true);
                 GameObject.Instantiate(bullet, gunTransform.position, gunTransform.rotation);
                 timeElapsed = Time.time + shootTime;
+
             }
+           
+            if (timeElapsed < Time.time && typeAttack == TypeAttack.Ray)
+            {
+                shootParticles.Play();
+                shootLight.SetActive(true);
+                Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
+                Ray ray = camGun.ScreenPointToRay(screenCenter);
+                if (Physics.Raycast(ray, out RaycastHit hitInfo, 100f,layerMask))
+                {
+
+                    if (hitInfo.collider.gameObject.GetComponent<Rigidbody>())
+                    {
+                        hitInfo.collider.gameObject.GetComponent<Rigidbody>().velocity = transform.forward * 10f;
+                    }
+                    timeElapsed = Time.time + shootTime;
+                }
+
+            }
+            
+        }
+        else
+        {
+            shootParticles.Stop();
+            shootLight.SetActive(false);
         }
     }
+
+    private void ShootEffect()
+    {
+        
+    }
+
 }
---
+++ FPSTest/Assets/Scripts/WeaponRecoil.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponRecoil : MonoBehaviour
+{
+    public GameObject gun;
+    public AnimationCurve recoilCurve;
+    public float recoilDuration = 0.1f;
+    public float recoilAmount = 5f;
+
+    private float timer = 0f;
+    private bool isRecoiling = false;
+    private Quaternion originalRotation;
+
+    void Start()
+    {
+        originalRotation = gun.transform.localRotation;
+    }
+
+    void Update()
+    {
+        if (isRecoiling)
+        {
+            timer += Time.deltaTime;
+            float normalizedTime = timer / recoilDuration;
+
+            if (normalizedTime >= 1f)
+            {
+                isRecoiling = false;
+                gun.transform.localRotation = originalRotation;
+                return;
+            }
+
+            float curveValue = recoilCurve.Evaluate(normalizedTime);
+            float recoil = curveValue * recoilAmount;
+
+            gun.transform.localRotation = originalRotation * Quaternion.Euler(-recoil, 0f, 0f); // e.g., cámara sube en Y
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            Fire();
+        }
+
+    }
+
+    void Fire()
+    {
+        timer = 0f;
+        isRecoiling = true;
+    }
+}
